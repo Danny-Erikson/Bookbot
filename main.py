6fa0c2880d38505882caf82ea62292f07cdd9078
Danny-Erikson <derikson413@gmail.com>
2025-10-13 12:36:39 -0700

    Added report functions

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
+# Funtions for analyzing books, See stats.py for more details
 from stats import count_words
 from stats import count_characters
+from stats import filter
 
 
 def get_book_text(path):
+    # Used to import a book to be analyzed
     with open(path) as f:
         return f.read()
 
 
 def main():
+    # Start of the promgram
+    print('============ BOOKBOT ============')
     text = get_book_text("./books/frankenstein.txt")
+    print(f'Analyzing book found at books/{"placeholder.txt"}')
+
+    # Word Count
     num_words = count_words(text)
+    print('----------- Word Count ----------')
     print(f'Found {num_words} total words')
+
+    # Character Count
+    print('--------- Character Count -------')
     char_dict = count_characters(text)
-    print(char_dict)
+    for i in filter(char_dict):  # *Loop through filtered characters
+        print(f'{i['char']}: {i['num']}')
+    print('============= END ===============')
 
 
+# Runtime
 if __name__ == "__main__":
     main()
