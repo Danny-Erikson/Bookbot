7dafcc38c14f6534b83792206240047f815b9b35
Danny-Erikson <derikson413@gmail.com>
2025-10-27 11:35:16 -0700

    Added filter slider

--- Bookbot++/Bookbot_app.py
+++ Bookbot++/Bookbot_app.py
@@ -13,6 +13,7 @@ class BookBotApp:
 
         # State variables
         self.file_path = None
+        self.filter_var = tk.DoubleVar(value=0)
 
         # *Create UI elements
         self._create_widgets()
@@ -20,22 +21,39 @@ class BookBotApp:
 
     def _create_widgets(self):
         """Create UI elements."""
+        # Create a bold font for labels
         bold_font = font.Font(weight="bold")
+
+        # *File selection
         self.file_select = ttk.Button(
             self.master, text="Select a File", command=self.select_file)
         self.file_label = ttk.Label(
             self.master, text="Selected file:", font=bold_font)
         self.file = ttk.Label(self.master)
 
+        # *Filter results
+        self.filter_label = ttk.Label(
+            self.master, text="Present to other 0%", font=bold_font)
+        self.filter_slider = ttk.Scale(
+            self.master, variable=self.filter_var, from_=0, to=100, length=150, orient="horizontal", command=self._update_filter_label)
+
     def _layout_widgets(self):
         """Pack/place/grid UI elements."""
-        self.file_select.pack(side=tk.TOP, pady=20)
-        self.file_label.pack(side=tk.LEFT, padx=(10, 2))
-        self.file.pack(side=tk.LEFT, padx=(2, 10))
+        self.file_select.pack()
+        self.file_label.pack()
+        self.file.pack()
+        self.filter_label.pack()
+        self.filter_slider.pack()
 
     def select_file(self):
+        """Open a file dialog to select a file."""
         path = filedialog.askopenfilename()
         if path:
             self.file_path = path
             file_name = path.split("/")[-1]
             self.file.config(text=file_name)
+
+    def _update_filter_label(self, event):
+        """Update the filter label when the slider is moved."""
+        self.filter_label.config(
+            text=f"Present to other: {int(self.filter_var.get())}%")
